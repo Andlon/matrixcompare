c7c328b270c1316dad44fc531e3721f6260957ab
Andreas Longva <andreas.b.longva@gmail.com>
2019-05-25 17:12:31 +0200

    Initial import from rulinalg

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+//#[macro_use]
+//mod assert_matrix_eq;
+//
+//#[macro_use]
+//mod assert_vector_eq;
+//
+//#[macro_use]
+//mod assert_scalar_eq;
+
+#[cfg(test)]
+#[macro_use]
+extern crate quickcheck;
+
+mod comparison;
+mod ulp;
+
+pub use self::comparison::{
+    AbsoluteElementwiseComparator,
+    ExactElementwiseComparator,
+    UlpElementwiseComparator,
+    FloatElementwiseComparator,
+
+    // The following are just imported because we want to
+    // expose trait bounds in the documentation
+    ElementwiseComparator
+};
+
+//pub use self::assert_matrix_eq::elementwise_matrix_comparison;
+//pub use self::assert_vector_eq::elementwise_vector_comparison;
+//pub use self::assert_scalar_eq::scalar_comparison;
