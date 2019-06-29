338416a984d9bd563b69bcf13fc4d943c6d11d8e
Andreas Longva <andreas.b.longva@gmail.com>
2019-06-29 19:00:22 +0200

    MockSparseMatrix

--- src/mock.rs
+++ src/mock.rs
@@ -1,10 +1,25 @@
-use crate::{Accessor, DenseAccessor, Matrix};
+use crate::{Accessor, DenseAccessor, Matrix, SparseAccessor};
 
 #[derive(Clone, Debug)]
 pub struct MockDenseMatrix<T> {
     data: Vec<T>,
     rows: usize,
     cols: usize,
+}
+
+#[derive(Clone, Debug)]
+pub struct MockSparseMatrix<T> {
+    shape: (usize, usize),
+    triplets: Vec<(usize, usize, T)>
+}
+
+impl<T> MockSparseMatrix<T> {
+    pub fn from_triplets(rows: usize, cols: usize, triplets: Vec<(usize, usize, T)>) -> Self {
+        Self {
+            shape: (rows, cols),
+            triplets
+        }
+    }
 }
 
 impl<T> MockDenseMatrix<T> {
@@ -36,5 +51,29 @@ impl<T: Clone> DenseAccessor<T> for MockDenseMatrix<T> {
     fn fetch_single(&self, row: usize, col: usize) -> T {
         let idx = row * self.cols + col;
         self.data[idx].clone()
+    }
+}
+
+impl<T: Clone> Matrix<T> for MockSparseMatrix<T> {
+    fn rows(&self) -> usize {
+        self.shape.0
+    }
+
+    fn cols(&self) -> usize {
+        self.shape.1
+    }
+
+    fn access(&self) -> Accessor<T> {
+        Accessor::Sparse(self)
+    }
+}
+
+impl<T: Clone> SparseAccessor<T> for MockSparseMatrix<T> {
+    fn nnz(&self) -> usize {
+        self.triplets.len()
+    }
+
+    fn fetch_triplets(&self) -> Vec<(usize, usize, T)> {
+        self.triplets.clone()
     }
 }
