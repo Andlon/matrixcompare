bd8ef9fe00ddd69e05da79f39b9892c8617b1a8b
Andreas Longva <andreas.b.longva@gmail.com>
2019-06-30 09:36:27 +0200

    Basic proptests for dense-sparse comparisons

--- tests/dense_sparse_matrix_comparion.rs
+++ tests/dense_sparse_matrix_comparion.rs
@@ -1,14 +1,24 @@
 use matrixcompare::compare_matrices;
 use matrixcompare::comparators::ExactElementwiseComparator;
-use matrixcompare_mock::{dense_matrix_strategy_i64, sparse_matrix_strategy_i64};
+use matrixcompare_mock::{dense_matrix_strategy_i64, sparse_matrix_strategy_i64,
+                         dense_matrix_strategy_normal_f64, sparse_matrix_strategy_normal_f64};
 use proptest::prelude::*;
 
 
 proptest! {
     #[test]
-    fn sparse_and_dense_matrices_should_compare_the_same_as_dense_dense(
+    fn sparse_and_dense_matrices_should_compare_the_same_as_dense_dense_i64(
         dense in dense_matrix_strategy_i64(0..5, 0..5),
         sparse in sparse_matrix_strategy_i64(0..5, 0..5)
+    ) {
+        let c = ExactElementwiseComparator;
+        compare_matrices(&dense, &sparse, &c) == compare_matrices(&dense, sparse.to_dense().unwrap(), &c)
+    }
+
+    #[test]
+    fn sparse_and_dense_matrices_should_compare_the_same_as_dense_dense_f64(
+        dense in dense_matrix_strategy_normal_f64(0..5, 0..5),
+        sparse in sparse_matrix_strategy_normal_f64(0..5, 0..5)
     ) {
         let c = ExactElementwiseComparator;
         compare_matrices(&dense, &sparse, &c) == compare_matrices(&dense, sparse.to_dense().unwrap(), &c)
