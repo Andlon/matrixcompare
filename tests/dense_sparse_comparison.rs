2cbb12df3dc204a5956ec61dd8256ed73ef0dc37
Andreas Longva <longva@cs.rwth-aachen.de>
2020-06-26 14:22:05 +0200

    Add some same size dense-sparse tests

--- tests/dense_sparse_comparison.rs
+++ tests/dense_sparse_comparison.rs
@@ -249,6 +249,15 @@ proptest! {
     }
 
     #[test]
+    fn same_size_sparse_and_dense_matrices_should_compare_the_same_as_dense_dense_i64(
+        (dense, sparse) in same_size_dense_sparse_matrices()
+    ) {
+        let c = ExactElementwiseComparator;
+        prop_assert_eq!(compare_matrices(&dense, &sparse, &c),
+                        compare_matrices(&dense, sparse.to_dense().unwrap(), &c));
+    }
+
+    #[test]
     fn sparse_and_dense_matrices_should_compare_the_same_as_dense_dense_f64(
         dense in dense_matrix_strategy_normal_f64(MATRIX_DIM_RANGE, MATRIX_DIM_RANGE),
         sparse in sparse_matrix_strategy_normal_f64(MATRIX_DIM_RANGE, MATRIX_DIM_RANGE)
@@ -262,6 +271,18 @@ proptest! {
     fn sparse_dense_comparison_is_symmetric_for_all_matrices_i64(
         dense in dense_matrix_strategy_i64(MATRIX_DIM_RANGE, MATRIX_DIM_RANGE),
         sparse in sparse_matrix_strategy_i64(MATRIX_DIM_RANGE, MATRIX_DIM_RANGE)
+    ) {
+        let c = ExactElementwiseComparator;
+        let result1 = compare_matrices(&dense, &sparse, &c);
+        let result2 = compare_matrices(&sparse, &dense, &c);
+
+        prop_assert_eq!(result1.clone(), result2.clone().map_err(|err| err.reverse()));
+        prop_assert_eq!(result1.map_err(|err| err.reverse()), result2);
+    }
+
+    #[test]
+    fn same_size_sparse_dense_comparison_is_symmetric_for_all_matrices_i64(
+        (dense, sparse) in same_size_dense_sparse_matrices()
     ) {
         let c = ExactElementwiseComparator;
         let result1 = compare_matrices(&dense, &sparse, &c);
