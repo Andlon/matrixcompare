366f1384a442240700c682a36b1e6671979ae56e
Andreas Longva <andreas.b.longva@gmail.com>
2021-01-13 11:03:55 +0100

    Add compilation of minimal hello world example to CI
    This prevents issue #3 from re-occurring.

---
+++ matrixcompare-minimal/src/main.rs
@@ -0,0 +1,8 @@
+//! This "minimal" crate is just a means of testing in CI that the most barebones
+//! example involving `matrixcompare` compiles. This is intended to prevent an issue like
+//! that reported in [issue #3](https://github.com/Andlon/matrixcompare/issues/3) from
+//! happening again.
+
+fn main() {
+    println!("Hello, world!");
+}
